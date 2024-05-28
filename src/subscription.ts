e892f3ae024f34f8324fa16cf6ce598f777bd2c6
dashuaibi45678 <1515611844@qq.com>
2024-05-28 23:12:14 +0800

    修改build文件和subscription文件

--- src/subscription.ts
+++ src/subscription.ts
@@ -6,7 +6,7 @@ import globalGroups from './globalGroups';
 export default defineGkdSubscription({
   id: 555,
   name: '大帅比45678的GKD订阅',
-  version: 1.0,
+  version: 0,
   author: '大帅比45678',
   checkUpdateUrl: './dashuaibi45678_gkd.version.json5',
   supportUri: 'https://github.com/dashuaibi45678/GKD-subscription/issues',

839be78530ca1ac3782f6a9b5ff78b383da47deb
dashuaibi45678 <1515611844@qq.com>
2024-05-30 23:45:18 +0800

    新增一些规则和注释

---
+++ src/apps/com.oray.sunlogin.ts
@@ -0,0 +1,28 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+    id: 'com.oray.sunlogin',
+    name: '向日葵远程控制',
+    groups: [
+        {
+            name: '开屏广告',
+            key: 0,
+            quickFind: true,
+            actionMaximum: 1,
+            order: -12,
+            matchTime: 10000,
+            resetMatch: 'app',
+            rules: [
+                {
+                    matches: [
+                        '[name="android.view.View"][visibleToUser=true][width=110][height=110]'
+                    ],
+                    snapshotUrls: [
+                        'https://i.gkd.li/i/15492782'
+                    ]
+                }
+            ]
+        }
+    ]
+}
+);
---
+++ src/apps/net.csdn.csdnplus.ts
@@ -0,0 +1,48 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+    id: 'net.csdn.csdnplus',
+    name: 'CSDN',
+    groups: [
+        {
+            name: '开屏广告',
+            key: 0,
+            actionMaximum: 1,
+            order: -12,
+            matchTime: 10000,
+            resetMatch: 'app',
+            rules: [
+                {
+                    matches: [
+                        '[text*="跳过"][text.length<=10]'
+                    ],
+                    quickFind: true,
+                    snapshotUrls: [
+                        'https://i.gkd.li/i/12673680'
+                    ]
+                },
+                {
+                    matches: [
+                        '@View <3 FrameLayout <2 FrameLayout < FrameLayout < [vid="fl_third_ad"]'
+                        //@View <n FrameLayout <n FrameLayout < FrameLayout <<n [vid="action_bar_root"]//这是另一种写法
+                        //[name="android.view.View"][visibleToUser=true][width=140][height=140]//也可以尝试寻找图形特征
+                        //@View <<n [vid="action_bar_root"]//甚至可以尝试这种写法
+                    ],
+                    quickFind: true,
+                    snapshotUrls: [
+                        'https://i.gkd.li/i/13826577'
+                    ]
+                },
+                {
+                    matches: [
+                        '[id$="tt_splash_skip_btn"]'
+                    ],
+                    snapshotUrls: [
+                        'https://i.gkd.li/i/13224627'
+                    ]
+                }
+            ]
+        }
+    ]
+}
+);

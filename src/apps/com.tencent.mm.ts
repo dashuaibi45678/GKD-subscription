8b11fd5879dcb366adfdde47c88321d0cef0a76d
dashuaibi45678 <1515611844@qq.com>
2024-06-01 14:39:55 +0800

    增加QQ和微信的规则

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -3,5 +3,136 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [],
+  groups: [
+    {
+      name: '开屏广告-微信小程序',
+      key: 0,
+      quickFind: true,
+      order: -12,
+      matchTime: 10000,
+      activityIds: [
+        'com.tencent.mm.plugin.appbrand.ui.AppBrandUI',
+        'com.tencent.mm.plugin.appbrand.launching.AppBrandLaunchProxyUI'
+      ],
+      rules: [
+        {
+          matches: [
+            '[text="广告"][visibleToUser=true]',
+            '[text="跳过"][visibleToUser=true]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/12785183',
+            'https://i.gkd.li/i/13306883',
+            'https://i.gkd.li/i/13407275',
+            'https://i.gkd.li/i/15108441'
+          ]
+        }
+      ]
+    },
+    {
+      name: '分段广告-公众号文章内部-广告 ∨',
+      key: 1,
+      enable: false,
+      quickFind: true,
+      activityIds: [
+        'com.tencent.mm.plugin.brandservice.ui.timeline.preload.ui.TmplWebViewMMUI'
+      ],
+      rules: [
+        {
+          key: 0,
+          action: 'clickCenter',
+          position: {
+            left: 'width*0.5',
+            top: 'height*0.5'
+          },
+          matches: [
+            '@View[visibleToUser=true] > TextView[text="广告"] + Image[text="feedback"] <<8 View'
+          ],
+          snapshotUrls: [
+            'https://m.gkd.li/55350790/1d60b7e4-6937-4b95-8b71-d7ab2b38adb9',
+            'https://m.gkd.li/55350790/236466b7-102f-47e5-bd88-2d69b8903434'
+          ]
+        },
+        {
+          key: 1,
+          preKeys: [
+            0
+          ],
+          action: 'clickCenter',
+          position: {
+            left: 'width*0.5',
+            top: 'height*0.5'
+          },
+          matches: [
+            '@TextView[text="关闭此广告"] < View[childCount=3]'
+          ],
+          snapshotUrls: [
+            'https://m.gkd.li/55350790/7ca6b000-3f1f-4604-a25c-04a9114a997e',
+            'https://m.gkd.li/55350790/2b249a65-c19e-40e0-82b4-7717f5f876f3'
+          ]
+        }
+      ]
+    },
+    {
+      name: '分段广告-公众号文章底部-广告 ∨',
+      key: 2,
+      desc: '在quickFind和activityIds之间添加“actionMaximum: 5”可限制执行次数',
+      enable: false,
+      quickFind: true,
+      activityIds: [
+        'com.tencent.mm.plugin.brandservice.ui.timeline.preload.ui.TmplWebViewMMUI',
+        'com.tencent.mm.plugin.webview.ui.tools.MMWebViewUI'
+      ],
+      rules: [
+        {
+          key: 0,
+          action: 'clickCenter',
+          position: {
+            left: 'width*0.5',
+            top: 'height*0.5'
+          },
+          matches: [
+            '@View[visibleToUser=true] > TextView[text="广告"] + Image[text*="feedback"] <<8 View'
+          ],
+          snapshotUrls: [
+            'https://m.gkd.li/55350790/236466b7-102f-47e5-bd88-2d69b8903434'
+          ]
+        },
+        {
+          key: 1,
+          preKeys: [
+            0
+          ],
+          action: 'clickCenter',
+          position: {
+            left: 'width*0.5',
+            top: 'height*0.5'
+          },
+          matches: [
+            '@TextView[text="不感兴趣"] < View[childCount=2]'
+          ],
+          snapshotUrls: [
+            'https://m.gkd.li/55350790/2b249a65-c19e-40e0-82b4-7717f5f876f3'
+          ]
+        },
+        {
+          key: 2,
+          preKeys: [
+            1
+          ],
+          action: 'clickCenter',
+          position: {
+            left: 'width*0.5',
+            top: 'height*0.5'
+          },
+          matches: [
+            '@TextView[text="与我无关"] < View[childCount=3]'
+          ],
+          snapshotUrls: [
+            'https://m.gkd.li/55350790/7c990a80-2217-459a-8f0f-78766b969644'
+          ]
+        }
+      ]
+    }
+  ]
 });
--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -3,5 +3,81 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mobileqq',
   name: 'QQ',
-  groups: [],
-});
+  groups: [
+    {
+      key: 0,
+      name: '分段广告-好友动态-广告 ×',
+      enable: false,
+      quickFind: true,
+      activityIds: [
+        'com.tencent.mobileqq.activity.SplashActivity',
+        'com.qzone.reborn.feedx.activity.QZoneFriendFeedXActivity'
+      ],
+      rules: [
+        {
+          key: 0,
+          matches: [
+            'View[desc="广告"][clickable=true] + @ImageView[desc="更多"][clickable=true]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/12929620',//不可用，自己的没传上去
+            'https://i.gkd.li/i/13387606'//不可用，自己的没传上去
+          ]
+        },
+        {
+          key: 1,
+          preKeys: [
+            0
+          ],
+          matches: [
+            'ImageView + @TextView[text="关闭此条广告"]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/12929619',//不可用，自己的没传上去
+            'https://i.gkd.li/i/13387605'//不可用，自己的没传上去
+          ]
+        }
+      ]
+    },
+    {
+      key: 1,
+      name: '分段广告-动态页-好友热播',
+      key: 5,
+      desc: '好友动态中的好友热播，自动选择“减少好友热播” - 默认关闭',
+      enable: false,
+      quickFind: true,
+      activityIds: [
+        'com.qzone.reborn.feedx.activity.QZoneFriendFeedXActivity',
+        'com.tencent.mobileqq.activity.SplashActivity'
+      ],
+      rules: [
+        {
+          name: '点击 [好友热播] 卡片右上角菜单按钮',
+          key: 0,
+          matches: [
+            '@Button[clickable=true] - [text="好友热播"]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/12721427',
+            'https://i.gkd.li/i/14822579'
+          ]
+        },
+        {
+          name: '点击[减少好友热播]',
+          key: 1,
+          preKeys: [
+            0
+          ],
+          matches: [
+            '@[clickable=true] >2 [text="减少好友热播"]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/12721433',
+            'https://i.gkd.li/i/14822576'
+          ]
+        }
+      ]
+    }
+  ]
+}
+);

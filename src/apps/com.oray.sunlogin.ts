bfcf3107cff61c09500f57a1fc57033867f75c1f
dashuaibi45678 <1515611844@qq.com>
2025-04-06 02:20:20 +0800

    Update com.oray.sunlogin.ts

--- src/apps/com.oray.sunlogin.ts
+++ src/apps/com.oray.sunlogin.ts
@@ -21,5 +21,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      name: '开屏广告',
+      key: 1,
+      quickFind: true,
+      actionMaximum: 1,
+      order: -12,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: [
+            '[text*="跳过"][text.length<10][visibleToUser=true]',
+          ],
+        },
+      ],
+    },
   ],
 });
